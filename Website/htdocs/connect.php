a06c04db6e4482cea9dad16a3b169cb059c524f8
gezgblack <gezgblack@gmail.com>
2015-09-08 14:44:20 +0100

    I added the website folder

---
+++ Website/htdocs/connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$user = 'root';
+$pass = '';
+$db = 'testdb';
+
+$db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
+
+echo"Connected to Database!!";
+?>
---
+++ Website/htdocs/index.php
@@ -0,0 +1,42 @@
+<html>
+	<head>
+		<Title>Home</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/index.css">
+	</head>
+
+	<body>
+		<div id="navigation">
+			
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Enroll</a></li>
+				<li><a href="#">Settings</a></li>
+				<li><a href="#">Logout</a></li>
+			</ul>
+		</div>
+		<div style="clear: both;"></div>
+		
+		<div id="day">
+				Today
+		</div>
+			
+		<div id="timetable">
+				
+			
+		</div>
+		
+
+
+	</body>
+
+
+</html>
+
+
+<!--
+SELECT class_time.start_time, class_time.end_time FROM class_time INNER JOIN class ON class_time.class_ID=class.class_ID INNER JOIN student_class ON class.class_ID=student_class.class_ID WHERE student_ID='X12345'
+
+
+
+
+-->
