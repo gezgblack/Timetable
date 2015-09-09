7306b161af7407286a47fc2ae428aaa935ba258a
gezgblack <gezgblack@gmail.com>
2015-09-09 20:11:16 +0100

    Added the login stuff

---
+++ Website/htdocs/login.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+	<title> Login </title>
+	<link rel="stylesheet" type="text/css" href="assets/css/login.css">
+</head>
+
+<body>
+	<div class="login-form">
+	<h1>Log In</h1>
+	
+		<form>
+	
+			<li>
+			<input type="text" value="Username" onfocus="this.value ='';">
+			</li>
+			
+			<li>
+			<input type="password" value="Password" onfocus="this.value ='';">
+			</li>
+			
+			<div class ="login-options">
+				<input type="submit" value = "Log In">
+				<input type="submit" value = "Register">
+			</div>
+		
+			
+			<div class ="forgot">
+				<h2><a href="#"> Forgot Password ? </a><h2>
+			</div>
+			
+		</form>
+	</div>
+
+</body>
+</html>
