7febf4bf0026f6f045bdf98a2ad690e6fb5823f4
gezgblack <gezgblack@gmail.com>
2015-09-11 14:14:29 +0100

    Scrapped the old login, new login is in a model form, the way we talked about at the start. Now it just needs to be implemented into the index

--- Website/htdocs/login.php
+++ Website/htdocs/login.php
@@ -1,39 +1,38 @@
+<!doctype html>
 <html>
 <head>
-	<title> Login </title>
-	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/login.css">
-	
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" media="all" href="assets/css/login.css">
+	<script type="text/javascript" src="assets/js/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" charset="utf-8" src="assets/js/jquery.leanModal.min.js"></script>
+	<!-- This is the JQuary plugin leanModal -->
 </head>
 
 <body>
-	<?php include 'navigation.php';?>
-	
-	<div class="login-form">
-	
-	
-		<form>
-	
-			<li>
-			<input type="text" value="Username" onfocus="this.value ='';">
-			</li>
-			
-			<li>
-			<input type="password" value="Password" onfocus="this.value ='';">
-			</li>
-			
-			<div class ="login-options">
-				<input type="submit" value = "Log In">
-				<input type="submit" value = "Register">
-			</div>
-		
-			
-			<div class ="forgot">
-				<h2><a href="#"> Forgot Password ? </a><h2>
-			</div>
-			
-		</form>
+	<div id="w">
+		<div id="content">
+			<center><a href="#loginmodal" class="flatbtn" id="modaltrigger">Login Button</a></center>
+		</div>
 	</div>
 
+	<div id="loginmodal" style="display:none;">
+	<form id="loginform" name="loginform" method="post" action="login.php">
+		<input type="text" name="username" id="username" class="txtfield" value="Username" onclick="this.value='';">
+		<input type="password" name="password" id="password" class="txtfield" value="Password" onclick="this.value='';">
+		<div class="center"><input type="submit" name="loginbtn" id="loginbtn" class="hidemodal" value="Log In"></div>
+	</form>
+	</div>
+	
+<script type="text/javascript">
+$(function(){
+  $('#loginform').submit(function(e){
+    return false;
+  });
+  
+  $('#modaltrigger').leanModal({ top: 110, overlay: 0.45, closeButton: ".hidemodal" });
+});
+</script>	
+	
 </body>
 </html>
